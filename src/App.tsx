0ea5e071cdabc3dd78f863bc2250504b8acff5f0
Brian <bbbrianboylen@gmail.com>
2021-05-08 16:34:49 -0500

    Added ts linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
-import { ApolloProvider, useQuery } from "@apollo/react-hooks";
-import config from "./aws-exports";
-console.log(config);
-const query = gql`
-  {
-    listBlogs {
-    nextToken
-  }
-  }
-`;
+import React from 'react';
+import './App.less';
+import { SpotifyPlayer } from './app/SpotifyPlayer';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { ApolloProvider } from '@apollo/react-hooks';
+import config from './aws-exports';
 
 const client = new ApolloClient({
   uri: config.aws_appsync_graphqlEndpoint,
   cache: new InMemoryCache(),
   headers: {
-    'x-api-key': config.aws_appsync_apiKey
+    'x-api-key': config.aws_appsync_apiKey,
   },
 });
 
 function App() {
-  const { loading, error, data } = useQuery(query);
-  if (loading) return <p>Loading...</p>;
-  if (error) {
-    console.log("error: ", error);
-    return <p>Error :(</p>;
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>{data.hello}</h1>
-      </header>
+      <SpotifyPlayer></SpotifyPlayer>
     </div>
   );
 }
 
-const AppWithProvider = () => (
+const AppWithProvider: React.FC = () => (
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>
---
+++ src/app/SpotifyPlayer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+
+// interface Props {}
+
+export const SpotifyPlayer = () => {
+  return (
+    <div className="Spotify-Player">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+};
